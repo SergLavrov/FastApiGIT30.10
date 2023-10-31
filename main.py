8bd2569ca9e96a4f88af6834ba31a5be4aa7939b
SergLavrov <ls500ls@mail.ru>
2023-10-31 19:49:22 +0300

    added get_project(id: int) (Чтение записи по ID)

--- main.py
+++ main.py
@@ -19,9 +19,10 @@ app = FastAPI()
 
 @app.get("/")
 def root():
-    return "Processing..."
+    return "Processing successful"
 
 
+# 1. Получение всех данных таблицы
 @app.get("/get-projects")
 def get_all_projects():
     try:
@@ -29,9 +30,23 @@ def get_all_projects():
         result = cursor.fetchall()
         responce = []
         for i in result:
-            responce.append({"id": i[0], "name": i[1], "lead_name": i[2], "count_user": [3], "is_finish": [4]})
+            responce.append({"id": i[0], "name": i[1], "lead_name": i[2], "count_user": i[3], "is_finish": i[4]})
         return responce
     except Exception as e:
         print(e)
         return e
 
+# 2. Чтение записи по ID
+@app.get("/get-project/{id}")
+def get_project(id: int):
+    try:
+        cursor.execute(f"SELECT * FROM project WHERE id = {id}")
+
+        result = cursor.fetchone()
+        return {"name": result[1], "lead_name": result[2], "count_user": result[3], "is_finish": result[4]}
+
+    except Exception as e:
+        print(e)
+        return e
+
+# 3. Добавление записи
